0ea5484967e4ca3d92d597847b76834e1f21a575
Paolo Artoni <paolo.artoni84@gmail.com>
2022-11-08 22:28:44 +0100

    corrected uncommit changes

--- GUI.py
+++ GUI.py
@@ -3,66 +3,97 @@ from myrec import onrec # import onrec function from myrec.py
 import transcribe #this is a local file called transcribe.py, we made it
 import torch
 import subprocess
+import threading
+import time
+import multiprocessing
 
-sg.theme("DarkTeal2")
-# in alto a destra c'e' il tasto per chiamare attenzione
-# Layout of the graphical interface
-layout = [[sg.Text("Record your thought")],
-          [sg.Graph(
-            canvas_size=(20, 20),
-            graph_bottom_left=(0, 0),
-            graph_top_right=(20, 20),
-            key="graph", 
-            background_color="red",
-            visible=False,
-            ),sg.Button("rec"), sg.Button("stop"), sg.Button("transcribe")], 
-          # add a separator in the gui
-          [sg.Text("",size=(50,10))],
-          [sg.Text("Choose from file")],
-          
-          [sg.Input(key="-IN2-" ,change_submits=True), sg.FileBrowse(key="-IN-")],
-          [sg.Button("Submit")],
-          [sg.Text("",size=(50,10))],
-          [sg.Multiline("Your thought:", key="thought", size=(50,10),)],
-          [sg.Button("save")],
-          [sg.Button("close")]] # key is used to get the value of the element, and use later
-              
+
+
+if __name__ == "__main__": 
     
-print("Is GPU available?",torch.cuda.is_available()) #check if GPU is available
-# Create the window
-window = sg.Window("Demo", layout, margins=(300, 300))
-
-# Create an event loop
-while True:
-    event, values = window.read() # Read the event that happened
-    # if rec button is pressed
-    if event == "rec":
-        window["graph"].update(visible=True)
-        window["thought"].update("Recording...")
-        onrec(window)
-        window["graph"].update(visible=False)
-    if event=="save":
-        print("save")
-        with open("file.txt", "w") as f:
-            f.write(window["thought"].get())
-    if event == "transcribe":
-        window["thought"].update("Transcribing, take a coffee...")
-        window.refresh() # refresh the window to show the new text above
-        result = transcribe.trans()
-        window["thought"].update(result["text"])
-        with open("file.txt", "w") as f:
-            f.write(result["text"])
-    if event == "Submit":
-        result = transcribe.trans(values["-IN-"])
-        window["thought"].update(result["text"])
-        with open("file.txt", "w") as f:
-            f.write(result["text"])
-        print(values["-IN-"])
+   
+    model = None
+    # manager = multiprocessing.Manager()
+    # return_dict = manager.dict()
+    sg.theme("DarkTeal2")
+    # in alto a destra c'e' il tasto per chiamare attenzione
+    # Layout of the graphical interface
+    layout = [[sg.Text('', size=(50, 1), relief='sunken', font=('Courier', 11),
+               text_color='yellow', background_color='black',key='TEXT')],
+              [sg.Text("Record your thought")],
+            [sg.Graph(
+                canvas_size=(20, 20),
+                graph_bottom_left=(0, 0),
+                graph_top_right=(20, 20),
+                key="graph", 
+                background_color="red",
+                visible=False,
+                ),sg.Button("rec"), sg.Button("stop"), sg.Button("transcribe")], 
+            # add a separator in the gui
+            [sg.Text("",size=(50,10))],
+            [sg.Text("Choose from file")],
+            
+            [sg.Input(key="-IN2-" ,change_submits=True), sg.FileBrowse(key="-IN-")],
+            [sg.Button("Submit")],
+            [sg.Text("",size=(50,10))],
+            [sg.Text("Time:", key="time", size=(30,1))],
+            [sg.Multiline("Your thought:", key="thought", size=(50,10),)],
+            [sg.Button("save")],
+            [sg.Button("close")]] # key is used to get the value of the element, and use later
+                
+    print("Is GPU available?",torch.cuda.is_available()) #check if GPU is available
+    x = threading.Thread(target=transcribe.preload, args=()) # create a thread
+    # x = multiprocessing.Process(target=transcribe.preload, args=(return_dict,)) # create a process
+    x.start()
+    # Create the window
+    window = sg.Window("Demo", layout, margins=(300, 300))
+    text = window['TEXT']
+    state = 0
+    model_loaded = False
+    # Create an event loop
+    while True:
+        event, values = window.read(timeout=20) # Read the event that happened
+        # if state reach the max value, reset it
+        if state < 50:
+            state = (state+1)%51 
+            text.update('█'*state)
+        else:
+            # make text green
+            text.update("Model loaded in GPU, ready to transcribe or rec", text_color='green')
+        # if rec button is pressed
+        if event == "rec":
+            window["graph"].update(visible=True)
+            window["thought"].update("Recording...")
+            onrec(window)
+            window["graph"].update(visible=False)
+        if event == "transcribe":
+            if model_loaded == False:
+                x.join()
+                model_loaded = True
+            start = time.time()
+            # print start time formatted
+            print("Start = {:.2f}".format(start))
+            window["thought"].update("Transcribing, take a coffee...")
+            window.refresh() # refresh the window to show the new text above
+            q1 = threading.Thread(target=transcribe.trans, args=(window, start)).start() # create a thread
+        if event == "Submit":
+            if model_loaded == False:
+                x.join()
+                model_loaded = True
+            start = time.time()
+            print("Start = {:.2f}".format(start))
+            q2 = threading.Thread(target=transcribe.trans, args=(window, start, values["-IN-"])).start() # create a thread
+            
         
-    
-    # End program if user closes window or
-    # presses the OK button
-    if event == "close" or event == sg.WIN_CLOSED:
-        break
+        if event=="save":
+            print("save")
+            with open("file.txt", "w") as f:
+                f.write(window["thought"].get())
+            
+        # End program if user closes window or
+        # presses the OK button
+        if event == "close" or event == sg.WIN_CLOSED:
+            break
 
-window.close()
+    window.close()
+    del(model)
--- transcribe.py
+++ transcribe.py
@@ -1,15 +1,25 @@
 import whisper
+import time 
 
-def trans(filepath="input.mp3")->None:
+def preload()->None:
+    global model # get the global variable model
+    model = whisper.load_model("large") #small, medium, large models are available
+    return model
+
+def trans(window, start,filepath="input.mp3")->None:
     """"
     This function transcribes the data from the whisper file to the console
     """
+    global model
     print("start transcribing")
     option = dict(language='Italian') # option is a dictionary, with the language as key, and Italian as value
-    model = whisper.load_model("large") #small, medium, large models are available
-    result = model.transcribe(filepath, **option) # transcribe the file output.mp3 to result (is a dictionary)
+    # model = whisper.load_model("large") #small, medium, large models are available
+    result = model.transcribe(filepath, **option) # transcribe the file input.mp3 to result (is a dictionary)
     print(result["text"])
-    
-    del(model) # delete the model from memory, otherwise it will be loaded in memory again and again for each call to this function
-    return result
+    window["thought"].update(result["text"]) #show the result
+    window["time"].update("Total time= %.2f sec" %((time.time() - start)))
+    print("end transcribe{:.2f}".format(time.time()) , " , Total time={:.2f}".format(time.time()-start))
+    # del(model) # delete the model from memory, otherwise it will be loaded in memory again and again for each call to this function
+    #return result
+
 
